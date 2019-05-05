6350b5836d84e03a4429c6d8032a588030597a3a
Santi <ivertroxmt@gmail.com>
2019-05-05 17:42:10 +0200

    Registro añadido

---
+++ ACOES JSF/src/java/Registro.java
@@ -0,0 +1,116 @@
+
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dave
+ */
+
+/**
+ *
+ * @author francis
+ */
+@Named(value = "registro")
+@javax.enterprise.context.RequestScoped
+public class Registro {
+    
+    private Socio registrar = new Socio();
+    
+    //@Inject
+    
+    //@EJB
+    private Socio socio;
+    
+    private String repass;
+
+    private String cuenta;
+    private String codigoValidacion;
+
+    private String mensajeValidacion;
+    private boolean validacionOK;
+
+    private boolean registroOK;
+
+    public boolean isRegistroOK() {
+        return registroOK;
+    }
+
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public String getCodigoValidacion() {
+        return codigoValidacion;
+    }
+
+    public void setCodigoValidacion(String codigoValidacion) {
+        this.codigoValidacion = codigoValidacion;
+    }
+
+    public Registro() {
+        socio = new Socio();
+    }
+
+    public String getRepass() {
+        return repass;
+    }
+
+    public void setRepass(String repass) {
+        this.repass = repass;
+    }
+
+    public Socio getSocio() {
+        return socio;
+    }
+
+    public void setUsuario(Socio usuario) {
+        this.socio = usuario;
+    }
+
+    public String registrarUsuario() {
+        if (!socio.getPassword().equals(repass)) {
+            FacesMessage fm = new FacesMessage("Las contraseñas deben coincidir");
+            FacesContext.getCurrentInstance().addMessage("registro:repass", fm);
+            return null;
+        }
+        //negocio.registrarUsuario(socio);
+        registroOK = true;
+        return "index.xhtml";
+    }
+
+    /*public String validarCuenta() {
+        try {
+            if (cuenta != null && codigoValidacion != null) {
+                negocio.validarCuenta(cuenta, codigoValidacion);
+            }
+            mensajeValidacion = "La validación ha sido correcta, ahora puede acceder con este usuario.";
+            validacionOK = true;
+        } catch (AgendaException e) {
+            mensajeValidacion = "Ha habido un error con la validación, compruebe que la URL es correcta.";
+            validacionOK = false;
+        }
+        return null;
+    }*/
+
+    public String getMensajeValidacion() {
+        return mensajeValidacion;
+    }
+
+    public boolean isValidacionOK() {
+        return validacionOK;
+    }
+
+}
